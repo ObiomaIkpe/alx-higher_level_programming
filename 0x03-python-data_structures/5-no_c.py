9c10e7d4508e8f0c3e760f17997343f109da8bf6
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-12-05 22:42:51 +0100

    no C or c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
 
     new_string = ""
     for char in my_string:
-        new_string += "" if char =="c" or char == "C" else char
+        new_string += "" if char == "c" or char == "C" else char
     my_string = new_string
     del new_string
     return (my_string)
