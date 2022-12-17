590dfc16cd7fae91026d6baf75d0fc4e38fd20a7
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-12-17 07:38:01 +0100

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+ #!/usr/bin/python3
+ for letter in range(97, 123):
+     print("{}".format(chr(letter)), end="")
