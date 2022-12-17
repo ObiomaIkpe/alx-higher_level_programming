d44736ff1c472710c92ec5d5d206ee807e70adee
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-12-17 07:42:45 +0100

    alphas

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
- #!/usr/bin/python3
- for letter in range(97, 123):
-     print("{}".format(chr(letter)), end="")
+#!/usr/bin/python3
+"""Function that prints all ASCII alphabet in lowercase, without a new line"""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
+
