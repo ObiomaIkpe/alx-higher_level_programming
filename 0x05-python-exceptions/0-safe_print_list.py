82cc3b716f5222c2b8e8f8e9c67606ab07382cc1
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-12-19 07:15:36 +0100

    typeError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     num_of_items = 0
 
     try:
-        for i  in range(x):
+        for i in range(x):
             print(my_list[i], end="")
             num_of_items += 1
     except IndexError:
