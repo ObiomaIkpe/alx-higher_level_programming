6db3fd7f39c71b0d978cb906961fa8bf57969ea9
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-12-19 12:42:13 +0100

    show lsit of integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i], end=""))
+            num_of_items += 1
         except (ValueError, TypeError):
-            pass
+            continue
+        print("")
     return (num_of_items)
