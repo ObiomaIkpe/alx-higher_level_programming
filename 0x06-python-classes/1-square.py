e1ae451c5c0422df2ca7493f26bfca4f83094d11
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-12-21 06:58:20 +0100

    Square with square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Creates a class square """
+
+
+class Square:
+    def __init__(self, size):
+        """ Initializes a new square
+
+        Args:
+            size(int): size of square
+            """
+        self.__size = size
