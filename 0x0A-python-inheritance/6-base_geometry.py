cfd30b4d060a453074f8bbbe545ac1fcd86d3923
ObiomaIkpe <obiomaikpe1@gmail.com>
2023-01-10 00:44:28 +0100

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Creates a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines an object based on based on 5-base_geometry.py"""
+
+    def area(self):
+        """Raises an Exception with message"""
+        raise Exception("area() is not implemented")
