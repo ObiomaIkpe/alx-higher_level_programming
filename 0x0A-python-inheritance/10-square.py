246da304f20e1cd8e172af5750215e5a059923b7
ObiomaIkpe <obiomaikpe1@gmail.com>
2023-01-10 00:57:34 +0100

    square#1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Creates a class Square"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Defines an object which is a subclass of `Rectangle`"""
+
+    def __init__(self, size):
+        """Initializes the square.
+
+        Args:
+            size (int): size of square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
