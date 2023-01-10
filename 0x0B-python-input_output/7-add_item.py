4528bb31968c68348cfc858dc5cce9a537d871ef
ObiomaIkpe <obiomaikpe1@gmail.com>
2023-01-11 00:02:17 +0100

    load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a Python list, and then saves them
+to a file"""
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+try:
+    args = load_from_json_file(filename)
+except FileNotFoundError:
+    args = []
+args += argv[1:]
+save_to_json_file(args, filename)
